a8cce4a7df5af504756b3b8c4ed7c6aec07ce20f
aashish <aashishrnjn8@gmail.com>
2025-09-01 18:11:07 +0400

    updated state file

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bucket"
+    bucket  = "rapyd-test-01-09-tfstate"
     key     = "envs/dev/terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
